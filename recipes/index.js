3fb2ccc5c309ae29934b9c271bbf038aa0807ed6
dijiflex <dijiflex@gmail.com>
2021-01-05 15:20:42 +0300

    connected cosmos mongodb API

--- recipes/index.js
+++ recipes/index.js
@@ -1,21 +1,58 @@
 
 
+const MongoClient = require('mongodb').MongoClient;
 
+const auth = {
+    user: 'recipeUser',
+    password: 'NLqHYhVOd1gsngPn'
+}
+
+const uri = process.env.dbConnectionString;
+
+let db = null;
+
+const loadDB = async () => {
+    try {
+        if (db) {
+            return db;
+        }
+        const client = new MongoClient(uri, { useNewUrlParser: true });
+        client.connect(err => {
+            db = client.db('reciperConnector');
+            // perform actions on the collection object
+            // client.close();
+        });
+
+        return db;
+    } catch (err) {
+        console.log(err.stack);
+    }
+
+}
 
 module.exports = async function (context, req) {
-    const recipes =  require('./recipes.json');
-    let res = '';
-    // const search = (req.query.search || (req.body && req.body.name));
-    const search = req.query.search;
-    if(req.query.search){
+    try {
+        const database = await loadDB();
+        let res = '';
+        // const search = (req.query.search || (req.body && req.body.name));
         const search = req.query.search;
-        res = recipes.filter(recipe => recipe.localSoupName === search);
-    } 
+        if (req.query.search) {
+            const search = req.query.search;
+            console.log(search);
+            res = recipes.filter(recipe => recipe.localSoupName === search);
+        } else {
+            res = await database.collection('recipes').find().toArray();
+        }
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: res
+        };
+    } catch (error) {
+        context.log(`Error code: ${error.code} message: ${error.message}`)
+        context.res = {
+            status: 500, /* Defaults to 200 */
+            body: { message: error.stack }
+        };
+    }
 
-    
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: res || recipes
-    };
 }
